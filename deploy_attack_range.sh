d7d6e03785d1e573ccf69ce2a713ae3469c05218
kseidenschnur <kseidenschnur@splunk.com>
2020-11-11 21:12:58 +0100

    add git

--- deploy_attack_range.sh
+++ deploy_attack_range.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update 
-sudo apt-get install -y python3-dev linux-headers-generic python-dev unzip python-pip vagrant virtualbox virtualbox-dkms python-virtualenv
+sudo apt-get install -y python3-dev linux-headers-generic python-dev unzip python-pip vagrant virtualbox virtualbox-dkms python-virtualenv git
 sudo gem install winrm-elevated
 sudo gem install winrm
 git clone https://github.com/splunk/attack_range_local && cd attack_range_local
